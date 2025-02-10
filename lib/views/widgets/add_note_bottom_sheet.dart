6b7bb50cd8dbd98b0f431230fee2c617922cc9a7
ibrahimsamir10 <ibrahimsamir179@gmail.com>
2025-02-11 00:46:02 +0200

    take input and validate data

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:store_app/views/widgets/custom_text_field.dart';
 
 import 'custom_bottom.dart';
@@ -8,27 +9,73 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const  Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          
-         children: [
-          SizedBox(height: 20,),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+final GlobalKey<FormState> formkey = GlobalKey();
+AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+String? title,subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return  Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+         const SizedBox(
+            height: 20,
+          ),
           CustomTextField(
+            onSaved: (value){
+              title = value;
+            },
             hint: 'Title',
           ),
-          SizedBox(height: 16,),
-           CustomTextField(
-            
+         const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onSaved: (value){
+              subtitle = value;
+            },
             hint: 'Content',
             maxlines: 5,
           ),
-          SizedBox(height: 32,),
-          CustomBottom(),
-           SizedBox(height: 16,),
-         ],
-        ),
+         const SizedBox(
+            height: 32,
+          ),
+          CustomBottom(
+            ontap: () {
+              if(formkey.currentState!.validate()){
+                formkey.currentState!.save();
+              } else{
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {
+                  
+                });
+              }
+            },
+          ),
+         const SizedBox(
+            height: 16,
+          ),
+        ],
       ),
     );
   }
--- lib/views/widgets/custom_bottom.dart
+++ lib/views/widgets/custom_bottom.dart
@@ -4,24 +4,27 @@ import 'package:flutter/material.dart';
 import 'constant.dart';
 
 class CustomBottom extends StatelessWidget {
-  const CustomBottom({super.key});
-
+  const CustomBottom({super.key, this.ontap});
+final void Function()? ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 55,
-      width: MediaQuery.of(context).size.width,
-     
-      decoration: BoxDecoration(
-         color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child:const Center(
-        child:  Text('Add',style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.bold
-        ),),
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        height: 55,
+        width: MediaQuery.of(context).size.width,
+       
+        decoration: BoxDecoration(
+           color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child:const Center(
+          child:  Text('Add',style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),),
+        ),
       ),
     );
   }
