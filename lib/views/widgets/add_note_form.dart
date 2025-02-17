c0a8df85bcea152571fc0bd933325ccf6fd6370a
ibrahimsamir10 <ibrahimsamir179@gmail.com>
2025-02-17 20:01:42 +0200

    change add note loading

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -47,20 +47,25 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 32,
           ),
-          CustomBottom(
-            ontap: () {
-              if (formkey.currentState!.validate()) {
-                formkey.currentState!.save();
-                var noteModel = NoteModel(
-                    title: title!,
-                    subtitle: subtitle!,
-                    date: DateTime.now().toString(),
-                    color: Colors.blue.value);
-                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomBottom(
+                isloading: state is AddNoteLoading ? true : false,
+                ontap: () {
+                  if (formkey.currentState!.validate()) {
+                    formkey.currentState!.save();
+                    var noteModel = NoteModel(
+                        title: title!,
+                        subtitle: subtitle!,
+                        date: DateTime.now().toString(),
+                        color: Colors.blue.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
             },
           ),
           const SizedBox(
--- lib/views/widgets/custom_bottom.dart
+++ lib/views/widgets/custom_bottom.dart
@@ -1,11 +1,11 @@
-
 import 'package:flutter/material.dart';
 
 import 'constant.dart';
 
 class CustomBottom extends StatelessWidget {
-  const CustomBottom({super.key, this.ontap});
-final void Function()? ontap;
+  const CustomBottom({super.key, this.ontap, this.isloading = false});
+  final void Function()? ontap;
+  final bool isloading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -13,17 +13,26 @@ final void Function()? ontap;
       child: Container(
         height: 55,
         width: MediaQuery.of(context).size.width,
-       
         decoration: BoxDecoration(
-           color: kPrimaryColor,
+          color: kPrimaryColor,
           borderRadius: BorderRadius.circular(8),
         ),
-        child:const Center(
-          child:  Text('Add',style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),),
+        child: Center(
+          child: isloading
+              ?const SizedBox(
+                height: 24,
+                width: 24,
+                child: const CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+              )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
         ),
       ),
     );
